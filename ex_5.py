f2b1f4167bd567fe216480058b6120614ef68a1a
Gadiididi <gadi.didi85@gmail.com>
2021-01-10 18:13:25 +0200

    test data added

--- ex_5.py
+++ ex_5.py
@@ -11,7 +11,7 @@ from gcommand_dataset import GCommandLoader
 cuda = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
-EPOCHS = 1
+EPOCHS = 40
 best_model = Net()
 best_model.to(device)
 
@@ -56,22 +56,22 @@ def test(val_loader):
         f"\n\tAccuracy: {correct}/{len(val_loader.dataset)} ({100. * correct / len(val_loader.dataset):.2f}%)\n")
 
 
-def prediction(test_loader):
+def prediction(test_loader, classes):
     best_model.eval()
     i = 0
     predicts_list = []
     with torch.no_grad():
         for image, labels in test_loader:
             image, labels = image.to(device), labels.to(device)
-            outputs = best_model(image)
-            _, predicted = torch.max(outputs.data, 1)
+            output = best_model(image)
+            predicted = output.data.max(1, keepdim=True)[1].item()
             data_ = int(test_loader.dataset.spects[i][0].split("\\")[2].split('.')[0])
             predicts_list.append((data_, predicted))
             i += 1
     predicts_list = sorted(predicts_list)
     f = open("test_y", "w")
     for e in predicts_list:
-        line = str(e[0]) + ".wav, " + str(e[1]) + '\n'
+        line = str(e[0]) + ".wav, " + classes[e[1]] + '\n'
         f.write(line)
     f.close()
 
@@ -82,12 +82,13 @@ def run_model(train_loader, val_loader, test_loader=None):
         train(train_loader)
         test(val_loader)
     if test_loader is not None:
-        prediction(test_loader)
+        classes = train_loader.dataset.classes
+        prediction(test_loader, classes)
 
 
 def main():
-    train_set = GCommandLoader("./short_train")
-    val_set = GCommandLoader("./short_valid")
+    train_set = GCommandLoader("./gcommands/train")
+    val_set = GCommandLoader("./gcommands/valid")
     test_set = GCommandLoader("./gcommands/test")
     train_loader = torch.utils.data.DataLoader(
         train_set, batch_size=64, shuffle=True,
